6c06235d06bd5af549bd6c5d5a38ce856eb9e69e
ganesh7995 <gundaganes040@gmail.com>
2025-06-18 18:52:11 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE (){
+    if [ $? -eq 0 ]
+    then
+        echo -e "$S2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$S2 is .. $G FAILED $N" | tee -a $LOG_FILE
+        exit 1
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo repos"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling Mongodb"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB"
+
+sed -i /127.0.0.1/0.0.0.0/g /etc/mongod.conf
+VALIDATE $? "Enabling remote Access"
+
+systemctl restart mongod &>>$LOG_FILE
+
+VALIDATE $? "MongoDB restarting"
