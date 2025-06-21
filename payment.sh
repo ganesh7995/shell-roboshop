cd8055f17db6b422088a1e7e759398daf599686f
ganesh7995 <gundaganes040@gmail.com>
2025-06-21 12:36:26 +0530

    roboshop

--- payment.sh
+++ payment.sh
@@ -35,7 +35,7 @@ VALIDATE(){
 }
 
 dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-VALIDATE $? "Install Python3 packages"
+VALIDATE $? "Installing Python3"
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
@@ -43,36 +43,35 @@ then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    echo -e "User already exist $Y SKIPPING $N"
 fi
+mkdir -p /app
+VALIDATE $? "creating app directory"
 
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading payment"
 
 rm -rf /app/*
 cd /app 
 unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "unzipping payment"
+VALIDATE $? "Unzipping the payment"
 
 pip3 install -r requirements.txt &>>$LOG_FILE
 VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-VALIDATE $? "Copying payment service"
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "Copying payment services"
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon Reload"
+VALIDATE $? "Daemon reloading"
 
 systemctl enable payment &>>$LOG_FILE
-VALIDATE $? "Enable payment"
+VALIDATE $? "Enabling payment"
 
 systemctl start payment &>>$LOG_FILE
 VALIDATE $? "Starting payment"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo -e "Script execution completed successfully, $Y time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
