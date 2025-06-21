a7df00ee318229a9537ed9b51b81f5a72d59a4df
ganesh7995 <gundaganes040@gmail.com>
2025-06-21 09:50:40 +0530

    roboshop

---
+++ user.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating roboshop user"
+else
+    echo -e "roboshop user already created $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Download user"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping user"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing npm"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "daemon reloading"
+
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enabling User"
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "Starting user"
+
+END_TIME=$(date+%S)
+
+TOTAL_TIME=$($END_TIME - $START_TIME)
+
+echo -e "Script execution completed, $Y tame taken:$TOTAL_TIME $N" &>>$LOG_FILE
